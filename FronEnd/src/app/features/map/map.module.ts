e1fbeaaf7cd5b590dfcddae6f39c3c3417f437af
julian carreno <julianctsistemas@gmail.com>
2024-06-13 12:06:58 -0500

    commit

--- FronEnd/src/app/features/map/map.module.ts
+++ FronEnd/src/app/features/map/map.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MapRoutingModule } from './map-routing.module';
 import { MapComponent } from './map.component';
-import { MapNavbarComponent } from './map-navbar/map-navbar.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    MapComponent,
-    MapNavbarComponent
+    MapComponent
   ],
   imports: [
     CommonModule,
