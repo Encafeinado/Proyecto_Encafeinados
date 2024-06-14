7669833133a633452f12ee89283422d4cbe89cd6
julian carreno <julianctsistemas@gmail.com>
2024-06-14 10:37:04 -0500

    formulario inicila de registro

--- FronEnd/src/app/auth/pages/register-page/register-page.component.ts
+++ FronEnd/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr'; // Importa ToastrService
+
+
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +13,33 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
-}
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService); // Inyecta ToastrService
+
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+  });
+
+
+  login() {
+    const { email, password } = this.myForm.value;
+  
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => {
+          // Mostrar Toastr con mensaje de éxito
+          this.toastr.success('¡Inicio de sesión exitoso!', 'Éxito');
+          // Redirigir al usuario a la página de la tienda
+          this.router.navigateByUrl('/store');
+        },
+        error: (message) => {
+          // Mostrar Toastr con mensaje de error en caso de fallo en el inicio de sesión
+          this.toastr.error(message, 'Error al iniciar sesión');
+        }
+      });
+    }
+  }
