d0b35020521eafd540725992f5631cdc2d4d65a9
Jotechno <jstamayotoro@hotmail.com>
2024-07-04 14:50:37 -0500

    Formulario

--- FronEnd/src/environments/environments.ts
+++ FronEnd/src/environments/environments.ts
@@ -1,6 +1,6 @@
 
 
 export const environment = {
-  //baseUrl: 'http://localhost:3000'
- baseUrl: 'https://encafeinados-backend.up.railway.app'
+  baseUrl: 'http://localhost:3000'
+//  baseUrl: 'https://encafeinados-backend.up.railway.app'
 }
