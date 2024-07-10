129f5baa90a93ca3961e4c846f6eec4f6e7ad1df
julian carreno <julianctsistemas@gmail.com>
2024-07-10 11:44:39 -0500

    fix

--- BackEnd/src/auth/auth.service.ts
+++ BackEnd/src/auth/auth.service.ts
@@ -39,24 +39,16 @@ export class AuthService {
       });
        await newUser.save();
       const {password:_,... user} = newUser.toJSON();
-      const newBook = new this.bookModel({
-        nameUser: user.name,
-        nameShop: '', // Este campo queda vacío
-        code: '', // Este campo queda vacío
-        status: true, // Ajustar según sea necesario
-        images: [] // Inicializa el array de imágenes vacío
-      });
-
-      await newBook.save();
 
       return user;
 
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(`${createUserDto.email} Ya Existe!`);
+      } catch (error) {
+       if(error.code === 11000){
+        throw new BadRequestException(`${createUserDto.email} Ya Existe!`)
+       }
+        throw new InternalServerErrorException('Algo terrible esta sucediendo!!!!')
       }
-      throw new InternalServerErrorException('Algo terrible está sucediendo!!!!');
-    }
+
   }
 
   async register(registerDto: RegisterUserDto): Promise<LoginResponce>{
