02be76f39c71bbe53f1648df778d600ada49e76e
julian carreno <julianctsistemas@gmail.com>
2024-07-27 16:12:35 -0500

    arreglo guard

--- FronEnd/src/app/auth/guards/is-authenticated.guard.ts
+++ FronEnd/src/app/auth/guards/is-authenticated.guard.ts
@@ -4,12 +4,31 @@ import { AuthService } from '../services/auth.service';
 import { AuthStatus } from '../interfaces';
 
 export const IsAuthenticatedGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-  const authService = inject( AuthService );
-  const router      = inject( Router );
+  const authStatus = authService.authStatus();
+  const currentUser = authService.currentUser();
 
-  if ( authService.authStatus() === AuthStatus.authenticated ) {
+  if (state.url === '/perfil') {
     return true;
+  }
+
+  if (authStatus === AuthStatus.authenticated) {
+    const expectedRole = route.data['role'] as string;
+    if (currentUser && currentUser.roles.includes(expectedRole)) {
+      return true;
+    } else {
+      // Redirige a la página correspondiente basada en el rol
+      if (currentUser && currentUser.roles.includes('user')) {
+        router.navigateByUrl('/map');
+      } else if (currentUser && currentUser.roles.includes('shop')) {
+        router.navigateByUrl('/store');
+      } else {
+        router.navigateByUrl('/landing');
+      }
+      return false;
+    }
   }
 
   router.navigateByUrl('/landing');
