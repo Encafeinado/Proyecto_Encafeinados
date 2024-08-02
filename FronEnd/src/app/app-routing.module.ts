c1fa30ef75bf3be1af7482443ad058cca8262359
julian carreno <julianctsistemas@gmail.com>
2024-08-02 14:25:21 -0500

    update landing store

--- FronEnd/src/app/app-routing.module.ts
+++ FronEnd/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'map', loadChildren: () => import('./features/map/map.module').then(m => m.MapModule), canActivate: [IsAuthenticatedGuard], data: { role: 'user' } },
   { path: 'landing', loadChildren: () => import('./features/landing/landing.module').then(m => m.LandingModule), canActivate: [IsNotAuthenticatedGuard] },
   { path: 'perfil', loadChildren: () => import('./features/mi-perfil/mi-perfil.module').then(m => m.MiPerfilModule), canActivate: [IsAuthenticatedGuard] },
+  { path: 'landing-tienda', loadChildren: () => import('./features/landing-tienda/landing-tienda.module').then(m => m.LandingTiendaModule), canActivate: [IsNotAuthenticatedGuard] },
   { path: '**', redirectTo: '/landing' }
 ];
 
