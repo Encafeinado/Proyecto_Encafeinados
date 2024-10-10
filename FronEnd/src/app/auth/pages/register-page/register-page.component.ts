e0a9f0ed3760bb3c5861645b853ab2f2bd71d9c9
EGaRcIa420 <133362145+EGaRcIa420@users.noreply.github.com>
2024-10-10 11:33:09 -0500

    Scroll the album, and register direccion

--- FronEnd/src/app/auth/pages/register-page/register-page.component.ts
+++ FronEnd/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,17 +1,31 @@
 // register-page.component.ts
-import { Component, inject, HostListener, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import {
+  Component,
+  inject,
+  HostListener,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 import { AuthService } from '../../services/auth.service';
 import { GeocodingService } from 'src/app/service/geocoding.service';
-import { emailDomainValidator, emailFormatAsyncValidator, passwordValidator, phoneNumberValidator, validateEmail, validateNameSimbolAndNumber } from '../../validators/custom-validators';
+import {
+  emailDomainValidator,
+  emailFormatAsyncValidator,
+  passwordValidator,
+  phoneNumberValidator,
+  validateEmail,
+  validateNameSimbolAndNumber,
+} from '../../validators/custom-validators';
 import { DataTreatmentDialogComponent } from 'src/app/data-treatment-dialog/data-treatment-dialog.component';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
-
-
-
 
 interface CustomFile {
   filename: string;
@@ -22,10 +36,9 @@ interface CustomFile {
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class RegisterPageComponent {
-
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
@@ -35,54 +48,85 @@ export class RegisterPageComponent {
   public showError = false; // Controla cuándo mostrar el mensaje de error
   public formSubmitted = false; // Nueva variable para controlar el envío del formulario
 
-  validDomains = ["gmail.com", "gmail.co", "gmail.es", "gmail.mx", "hotmail.com", "hotmail.co", "hotmail.es", "hotmail.mx", "outlook.com", "outlook.co", "outlook.es", "outlook.mx", "yahoo.com", "yahoo.co", "yahoo.es",
-    "yahoo.mx", "gmail.com.co", "hotmail.com.co", "outlook.com.co", "yahoo.com.co", "gmail.com.es", "hotmail.com.es", "outlook.com.es", "yahoo.com.es", "gmail.com.mx", "hotmail.com.mx", "outlook.com.mx",
-    "yahoo.com.mx","yopmail.com","@icloud.com"];
+  validDomains = [
+    'gmail.com',
+    'gmail.co',
+    'gmail.es',
+    'gmail.mx',
+    'hotmail.com',
+    'hotmail.co',
+    'hotmail.es',
+    'hotmail.mx',
+    'outlook.com',
+    'outlook.co',
+    'outlook.es',
+    'outlook.mx',
+    'yahoo.com',
+    'yahoo.co',
+    'yahoo.es',
+    'yahoo.mx',
+    'gmail.com.co',
+    'hotmail.com.co',
+    'outlook.com.co',
+    'yahoo.com.co',
+    'gmail.com.es',
+    'hotmail.com.es',
+    'outlook.com.es',
+    'yahoo.com.es',
+    'gmail.com.mx',
+    'hotmail.com.mx',
+    'outlook.com.mx',
+    'yahoo.com.mx',
+    'yopmail.com',
+    '@icloud.com',
+  ];
 
-    public myForm: FormGroup = this.fb.group({
-      name: ['', 
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
         {
           validators: [
             Validators.required,
             Validators.maxLength(30),
             Validators.minLength(3),
-            validateNameSimbolAndNumber()  // Validación síncrona
-        ], 
-          updateOn: 'change'  // Se actualiza cuando pierde el foco
-        }
-      ],
-    
-      email: ['', 
-        {
-          validators: [
-            Validators.required,
-          ], 
-          asyncValidators: [
-            emailDomainValidator(this.validDomains)
+            validateNameSimbolAndNumber(), // Validación síncrona
           ],
-          updateOn: 'change'  // Se actualiza cuando pierde el foco
-        }
+          updateOn: 'change', // Se actualiza cuando pierde el foco
+        },
       ],
-    
-      password: ['', 
+
+      email: [
+        '',
+        {
+          validators: [Validators.required],
+          asyncValidators: [emailDomainValidator(this.validDomains)],
+          updateOn: 'change', // Se actualiza cuando pierde el foco
+        },
+      ],
+
+      password: [
+        '',
         {
           validators: [
             Validators.required,
             Validators.minLength(8),
-            passwordValidator()
+            passwordValidator(),
           ],
-          updateOn: 'change'  // Se actualiza cuando pierde el foco
-        }
+          updateOn: 'change', // Se actualiza cuando pierde el foco
+        },
       ],
-    
-      confirmPassword: ['', 
+
+      confirmPassword: [
+        '',
         {
           validators: [Validators.required],
-          updateOn: 'change'  // Se actualiza cuando pierde el foco
-        }
+          updateOn: 'change', // Se actualiza cuando pierde el foco
+        },
       ],
-    
-      phone: ['', 
+
+      phone: [
+        '',
         {
           validators: [
             Validators.required,
@@ -90,19 +134,20 @@ export class RegisterPageComponent {
             Validators.minLength(10),
             Validators.maxLength(10),
           ],
-          updateOn: 'change'  // Se actualiza cuando pierde el foco
-        }
+          updateOn: 'change', // Se actualiza cuando pierde el foco
+        },
       ],
-    
+
       specialties1: ['', { updateOn: 'blur' }],
       specialties2: ['', { updateOn: 'blur' }],
       address: ['', { updateOn: 'blur' }],
       logo: ['', { updateOn: 'blur' }],
-      statusShop: [false]  // Campo oculto con valor predeterminado, sin `updateOn`
-    }, {
-      validators: this.passwordMatchValidator
-    });
-    
+      statusShop: [false], // Campo oculto con valor predeterminado, sin `updateOn`
+    },
+    {
+      validators: this.passwordMatchValidator,
+    }
+  );
 
   public isUser: boolean = true;
   public isStore: boolean = false;
@@ -141,7 +186,7 @@ export class RegisterPageComponent {
         this.logoFile = {
           filename: file.name,
           filetype: file.type,
-          value: reader.result
+          value: reader.result,
         };
         this.myForm.patchValue({ logo: reader.result });
       };
@@ -157,19 +202,29 @@ export class RegisterPageComponent {
     this.hidePasswordconfirm = !this.hidePasswordconfirm;
   }
 
-
   passwordMatchValidator(group: FormGroup): ValidationErrors | null {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { mismatch: true };
   }
-  
+
   onAddressChange(event: any) {
-    const query = event.target.value;
-    if (query.length > 2) {
-      this.geocodingService.autocompleteAddress(query).subscribe((response) => {
-        this.suggestedAddresses = response.map((result: any) => result.display_name);
+    const address = event.target.value;
+
+    if (address.length > 2) {
+      this.geocodingService.geocodeAddress(address).subscribe({
+        next: (results: any) => {
+          // Suponiendo que el servicio devuelve una lista de sugerencias de direcciones
+          this.suggestedAddresses = results.map(
+            (result: any) => result.formatted_address
+          ); // Asumiendo que devuelve 'formatted_address'
+        },
+        error: (err) => {
+          console.error('Error al obtener sugerencias de direcciones:', err);
+        },
       });
+    } else {
+      this.suggestedAddresses = [];
     }
   }
 
@@ -184,7 +239,6 @@ export class RegisterPageComponent {
       this.showError = false; // Ocultar el error si el formulario es válido
     }
   }
-  
 
   register() {
     if (this.myForm.invalid) {
@@ -193,53 +247,83 @@ export class RegisterPageComponent {
     }
 
     // Abre el diálogo y espera la respuesta
-    const dialogRef = this.MatDialogModule.open(DataTreatmentDialogComponent,{
-      disableClose: true 
+    const dialogRef = this.MatDialogModule.open(DataTreatmentDialogComponent, {
+      disableClose: true,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        const { name, email, password, phone, specialties1, specialties2, address, logo } = this.myForm.value;
+        const {
+          name,
+          email,
+          password,
+          phone,
+          specialties1,
+          specialties2,
+          address,
+          logo,
+        } = this.myForm.value;
 
         if (this.isStore) {
           this.geocodingService.geocodeAddress(address).subscribe({
-            next: (response) => {
-              if (response.length > 0) {
-                const { lat, lon } = response[0];
+            next: (results: google.maps.GeocoderResult[]) => {
+              if (results.length > 0) {
+                const location = results[0].geometry.location;
+                const lat = location.lat();
+                const lon = location.lng();
                 console.log('Latitud:', lat, 'Longitud:', lon);
                 console.log('Dirección: ', address);
-                this.authService.registerStore(name, email, password, phone, specialties1, specialties2, address, logo, lat, lon, false)
+
+                this.authService
+                  .registerStore(
+                    name,
+                    email,
+                    password,
+                    phone,
+                    specialties1,
+                    specialties2,
+                    address,
+                    logo,
+                    lat,
+                    lon,
+                    false
+                  )
                   .subscribe({
                     next: () => {
-                      this.toastr.success('¡Registro de tienda exitoso!', 'Éxito');
+                      this.toastr.success(
+                        '¡Registro de tienda exitoso!',
+                        'Éxito'
+                      );
                       this.router.navigateByUrl('/auth/login');
                     },
                     error: (err) => {
                       console.error('Error al registrar tienda:', err);
                       this.toastr.error('Error al registrar tienda', 'Error');
-                    }
+                    },
                   });
               } else {
-                this.toastr.error('No se pudo geocodificar la dirección', 'Error');
+                this.toastr.error(
+                  'No se pudo geocodificar la dirección',
+                  'Error'
+                );
               }
             },
             error: (err) => {
               console.error('Error al geocodificar la dirección:', err);
               this.toastr.error('Error al geocodificar la dirección', 'Error');
-            }
+            },
           });
         } else {
-          this.authService.register(name, email, password, phone)
-            .subscribe({
-              next: () => {
-                this.toastr.success('¡Registro de usuario exitoso!', 'Éxito');
-                this.router.navigateByUrl('/auth/login');
-              },
-              error: (err) => {
-                console.error('Error al registrar usuario:', err);
-                this.showError = true;
-              }
-            });
+          this.authService.register(name, email, password, phone).subscribe({
+            next: () => {
+              this.toastr.success('¡Registro de usuario exitoso!', 'Éxito');
+              this.router.navigateByUrl('/auth/login');
+            },
+            error: (err) => {
+              console.error('Error al registrar usuario:', err);
+              this.showError = true;
+            },
+          });
         }
       } else {
         // Usuario no aceptó el tratamiento de datos
@@ -248,12 +332,13 @@ export class RegisterPageComponent {
     });
   }
 
-
-
   @HostListener('document:click', ['$event'])
   onClick(event: MouseEvent) {
     const target = event.target as HTMLElement;
-    if (!target.closest('.wrap-input100') && !target.closest('.address-suggestions')) {
+    if (
+      !target.closest('.wrap-input100') &&
+      !target.closest('.address-suggestions')
+    ) {
       this.suggestedAddresses = [];
     }
   }
