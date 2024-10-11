e9ad6954af8212fba1e91b19ec53ba3433bd9292
“Alejandro <alejandro.sarmiento@ikitech.com.co>
2024-10-11 10:17:52 -0500

    cm: update environments.ts

--- FronEnd/src/environments/environments.ts
+++ FronEnd/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://encafeinados.club//encafeinados-api'
+  baseUrl: 'https://encafeinados.club/encafeinados-api'
 //  baseUrl: 'https://encafeinados-backend.up.railway.app'
 }
