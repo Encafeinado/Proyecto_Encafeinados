e47cd046544865bebd419630f57e639cbe55da18
EGaRcIa420 <133362145+EGaRcIa420@users.noreply.github.com>
2024-11-01 11:41:11 -0500

    Changes billing list with payment of the shop

--- FronEnd/src/app/features/billing-list/billing-list.component.ts
+++ FronEnd/src/app/features/billing-list/billing-list.component.ts
@@ -11,6 +11,8 @@ export class BillingListComponent implements OnInit, OnDestroy {
   monthDropdownOpen: boolean = false; // Estado para el dropdown del mes
   storeDropdownOpen: boolean = false; // Estado para el dropdown de tienda
   selectedStoreId: string | null = null; // ID de la tienda seleccionada
+  selectedStatus: boolean | null = null; // Cambiar a boolean
+  statusDropdownOpen: boolean = false; // Controla la visibilidad del dropdown
   codesUsedInMonth: number = 0;
   filteredCodes: { code: string; value: number; status: string }[] = [];
   selectedYear: number | null = null;
@@ -39,6 +41,11 @@ export class BillingListComponent implements OnInit, OnDestroy {
   pageSize: number = 5;
   totalPayments: number = 0;
   totalPages: number = 0;
+  pendingPaymentsCount: number = 0; // Asegúrate de agregar esto en tu clase
+  amountPayments: number = 0;
+  formattedAmountPayments: string = '';
+  pendingStores: any[] = [];
+  startDate: Date = new Date(2024, 9, 1);
 
   constructor(private storeService: StoreService) {}
 
@@ -46,6 +53,7 @@ export class BillingListComponent implements OnInit, OnDestroy {
     this.loadYears();
     this.loadStores();
     this.loadPayments();
+    this.loadPendingPayments();
     document.addEventListener('click', this.handleClickOutside.bind(this));
   }
 
@@ -76,6 +84,16 @@ export class BillingListComponent implements OnInit, OnDestroy {
     this.storeDropdownOpen = !this.storeDropdownOpen;
     this.yearDropdownOpen = false; // Cierra el dropdown del año si está abierto
     this.monthDropdownOpen = false; // Cierra el dropdown del mes si está abierto
+  }
+
+  toggleStatusDropdown() {
+    this.statusDropdownOpen = !this.statusDropdownOpen;
+  }
+
+  selectStatus(status: boolean) {
+    this.selectedStatus = status; // Ahora selecciona true o false
+    this.statusDropdownOpen = false;
+    this.onFilterChange();
   }
 
   selectStore(storeId: string): void {
@@ -112,9 +130,13 @@ export class BillingListComponent implements OnInit, OnDestroy {
     this.storeService.getAllPayments().subscribe(
       (data: any) => {
         this.payments = data;
+        // Cargar las tiendas pendientes después de obtener los pagos
+        this.loadPendingPayments(); // Llama a este método para llenar pendingStores
         this.filteredPayments = this.payments;
         this.totalPayments = this.filteredPayments.length;
         this.calculateTotalPages();
+        this.countPendingPayments();
+        this.amountPaymentsShop();
         console.log('Pagos cargados:', data);
       },
       (error) => {
@@ -122,6 +144,54 @@ export class BillingListComponent implements OnInit, OnDestroy {
       }
     );
   }
+  
+
+  loadPendingPayments(): void {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1; // Mes actual (1 a 12)
+  
+    this.storeService.getAllShops().subscribe(
+      (shops: any[]) => {
+        this.storeService.getAllPayments().subscribe(
+          (payments: any[]) => {
+            this.pendingStores = shops.filter((shop) => {
+              const shopPayments = payments.filter(
+                (payment) => payment.shopId === shop._id && payment.year === currentYear
+              );
+  
+              const startMonth = this.startDate.getMonth() + 1;
+              const missingMonths = [];
+  
+              // Revisamos solo desde el mes de inicio hasta el mes actual
+              for (let month = startMonth; month <= currentMonth; month++) {
+                const hasPaymentForMonth = shopPayments.some(
+                  (payment) => payment.month === month
+                );
+  
+                if (!hasPaymentForMonth) {
+                  missingMonths.push(month); // Agrega el mes si falta el pago
+                }
+              }
+  
+              // Si tiene meses faltantes, se agrega como pendiente
+              if (missingMonths.length > 0) {
+                shop.missingMonths = missingMonths; // Añadir meses faltantes a la tienda
+                return true;
+              }
+              return false;
+            });
+            console.log('Tiendas con pagos pendientes:', this.pendingStores);
+          },
+          (error) => {
+            console.error('Error al cargar pagos:', error);
+          }
+        );
+      },
+      (error) => {
+        console.error('Error al cargar tiendas:', error);
+      }
+    );
+  }  
 
   onFilterChange(): void {
     this.filterPayments();
@@ -138,6 +208,7 @@ export class BillingListComponent implements OnInit, OnDestroy {
       storeId: this.selectedStoreId,
       year: this.selectedYear,
       month: this.selectedMonth,
+      status: this.selectedStatus,
     });
 
     this.filteredPayments = this.payments.filter((payment) => {
@@ -148,8 +219,11 @@ export class BillingListComponent implements OnInit, OnDestroy {
         payment.month === parseInt(this.selectedMonth, 10);
       const shopMatches =
         !this.selectedStoreId || payment.shopId === this.selectedStoreId;
+      const statusMatches =
+        this.selectedStatus === null ||
+        payment.statusPayment === this.selectedStatus; // Compara con true o false
 
-      return yearMatches && monthMatches && shopMatches;
+      return yearMatches && monthMatches && shopMatches && statusMatches;
     });
 
     console.log('Pagos filtrados:', this.filteredPayments);
@@ -200,6 +274,7 @@ export class BillingListComponent implements OnInit, OnDestroy {
     const yearSelect = document.querySelector('.year-select');
     const monthSelect = document.querySelector('.month-select');
     const storeSelect = document.querySelector('.store-select'); // Agrega esta línea
+    const statusSelect = document.querySelector('.status-select'); // Agrega esta línea
 
     // Cierra el dropdown del año si se hace clic fuera de él
     if (this.yearDropdownOpen && yearSelect && !yearSelect.contains(target)) {
@@ -223,5 +298,60 @@ export class BillingListComponent implements OnInit, OnDestroy {
     ) {
       this.storeDropdownOpen = false;
     }
+
+    // Cierra el dropdown de estado si se hace clic fuera de él
+    if (
+      this.statusDropdownOpen &&
+      statusSelect &&
+      !statusSelect.contains(target)
+    ) {
+      this.statusDropdownOpen = false;
+    }
+  }
+
+  countPendingPayments() {
+    this.pendingPaymentsCount = this.payments.filter(
+      (payment) => payment.statusPayment === false
+    ).length;
+    console.log(this.pendingPaymentsCount);
+  }
+
+  amountPaymentsShop() {
+    this.amountPayments = this.payments.reduce((total, payment) => {
+      return total + (payment.amount || 0); // Asegúrate de que 'amount' sea el nombre correcto
+    }, 0);
+
+    // Formatea el monto a pesos colombianos
+    this.formattedAmountPayments = new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+    }).format(this.amountPayments);
+
+    console.log(this.amountPayments);
+    console.log(this.formattedAmountPayments); // Para verificar el formato
+  }
+
+  clearSelectedStore(event: MouseEvent): void {
+    event.stopPropagation(); // Previene que el clic cierre el dropdown
+    this.selectedStoreId = null; // Limpia la selección de la tienda
+    this.onFilterChange(); // Aplica los cambios
+  }
+
+  clearSelectedYear(event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectedYear = null;
+    this.onFilterChange();
+  }
+
+  clearSelectedMonth(event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectedMonth = null;
+    this.onFilterChange();
+  }
+
+  clearSelectedStatus(event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectedStatus = null;
+    this.onFilterChange();
   }
 }
