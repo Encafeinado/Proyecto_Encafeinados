544f863830ae4fafaaf73f12e6d3d25e3281cd68
julian carreno <julianctsistemas@gmail.com>
2024-11-14 17:07:44 -0500

    change error

--- FronEnd/src/app/app-routing.module.ts
+++ FronEnd/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { IsNotAuthenticatedGuard } from './auth/guards/is-not-authenticated.guar
 import { IsAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' }, // Redirigir cuando no hay ruta específica
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [IsNotAuthenticatedGuard] },
   { path: 'store', loadChildren: () => import('./features/store/store.module').then(m => m.StoreModule), canActivate: [IsAuthenticatedGuard], data: { role: 'shop' } },
   { path: 'map', loadChildren: () => import('./features/map/map.module').then(m => m.MapModule), canActivate: [IsAuthenticatedGuard], data: { role: 'user' } },
@@ -12,10 +13,9 @@ const routes: Routes = [
   { path: 'payment', loadChildren: () => import('./features/payment/payment.module').then(m => m.PaymentModule) },
   { path: 'billing-list', loadChildren: () => import('./features/billing-list/billing-list.module').then(m=> m.BillingListModule)},
   { path: 'landing-tienda', loadChildren: () => import('./features/landing-tienda/landing-tienda.module').then(m => m.LandingTiendaModule), canActivate: [IsNotAuthenticatedGuard] },
-  { path: 'admin-profile', loadChildren: () => import('./features/admin-profile/admin-profile.module').then(m => m.AdminProfileModule),canActivate: [IsAuthenticatedGuard], data: { role: 'admin' }}, 
+  { path: 'admin-profile', loadChildren: () => import('./features/admin-profile/admin-profile.module').then(m => m.AdminProfileModule), canActivate: [IsAuthenticatedGuard], data: { role: 'admin' }}, 
   
-  
-  // { path: '**', redirectTo: '/landing' }
+  { path: '**', redirectTo: 'landing' } // Redirigir rutas desconocidas a /landing
 ];
 
 @NgModule({
